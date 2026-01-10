e477f799dace33d7afc828afdc89e6340dc854cf
adarsh0008 <adarshlkj@gmail.com>
2026-01-10 20:13:14 +0530

    login with google backend

--- server.mjs
+++ server.mjs
@@ -3,7 +3,6 @@ import Fastify from "fastify";
 import cors from "@fastify/cors";
 import jwt from "@fastify/jwt";
 import oauthPlugin from "@fastify/oauth2";
-import dotenv from "dotenv";
 
 dotenv.config();
 
